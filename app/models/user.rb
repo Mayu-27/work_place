725f0216ef1b625e4ea3efec7255bac33c29a137
Mayu-27 <62289802+Mayu-27@users.noreply.github.com>
2021-03-03 22:57:25 +0900

    Userモデルのバリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,27 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: { case_sensitive: true }
+    validates :password, length: { minimum: 6 }
+    validates :password, confirmation: true
+    validates :password_confirmation
+    validates :birth_date
+  end
+      
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+      
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+      
+  with_options presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: '全角カタカナのみで入力して下さい' } do
+    validates :kana_last_name
+    validates :kana_first_name
+  end
+  
   has_many :reviews
 end
