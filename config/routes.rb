f71599ac9955b7b847e820ede321687b20066291
Mayu-27 <62289802+Mayu-27@users.noreply.github.com>
2021-03-13 15:57:31 +0900

    ビューの調整

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   resources :users, only: [:show] 
   resources :spots do
     resource :favorites, only: [:create, :destroy]
-    resources :reviews, only: [:index, :new, :create, :show]
-    post 'review' => 'reviews#create'
+    resources :reviews, only: [:index, :new, :create]
+    # post 'review' => 'reviews#create'
   end
 end
 
