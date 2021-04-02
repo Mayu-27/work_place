0abe3c92d8b3d2fba60d6a23d24b818576797b7f
Mayu-27 <62289802+Mayu-27@users.noreply.github.com>
2021-04-02 23:48:12 +0900

    ヘッダのCSS修正

---
+++ app/javascript/preview.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const ImageList = document.getElementById('preview-image');
+  document.getElementById('preview-image').addEventListener('change', function(e){
+    console.log(123);
+  });
+});
