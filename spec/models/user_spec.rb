9f7b4e4426f1d323845b9d558fecc04cfc4e9988
Mayu-27 <62289802+Mayu-27@users.noreply.github.com>
2021-04-15 13:26:25 +0900

    factorybotの導入

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,110 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    context '新規登録できるとき' do
+      it 'nickname,email,password,password_confirmation,last_name, first_name, kana_last_name, kana_first_name, birth_dateが存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+    context '新規登録できないとき' do
+      it 'nicknameが空では登録できない' do
+        @user.nickname = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("ニックネームを入力してください")
+      end
+      it 'emailが空では登録できない' do
+        @user.email = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Eメールを入力してください")
+      end
+      it 'passwordが空では登録できない' do
+        @user.password = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードを入力してください")
+      end
+      it 'passwordとpassword_confirmationが一致しなければ登録できない' do
+        @user.password_confirmation = '00000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
+      end
+      it 'last_nameが空では登録できない' do
+        @user.last_name = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("姓を入力してください")
+      end
+      it 'first_nameが空では登録できない' do
+        @user.first_name = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("名前を入力してください")
+      end
+      it 'kana_last_nameが空では登録できない' do
+        @user.kana_last_name = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("姓（カナ）を入力してください")
+      end
+      it 'kana_first_nameが空では登録できない' do
+        @user.kana_first_name = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("名前（カナ）を入力してください")
+      end
+      it 'birth_dateが空では登録できない' do
+        @user.birth_date = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("生年月日を入力してください")
+      end
+      it '重複したemailが存在する場合登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Eメールはすでに存在します')
+      end
+      it 'passwordが6文字以上でないと登録できない' do
+        @user.password = 'test0'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('パスワードは6文字以上で入力してください')
+      end
+      it 'passwordが英字のみだと登録できない' do
+        @user.password = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('パスワードには英字と数字の両方を含めて設定してください')
+      end
+      it 'passwordが数字のみだと登録できない' do
+        @user.password = '000000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('パスワードには英字と数字の両方を含めて設定してください')
+      end
+      it "パスワードと確認が一致していないと登録できない" do
+        @user.password_confirmation = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end   
+      it 'emailに@が含まれていないと登録できない' do
+        @user.email = 'testgmail.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Eメールは不正な値です')
+      end
+      it 'last_nameは漢字・カタカナ・ひらがなでないと登録できない' do
+        @user.last_name = '000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('姓全角文字を使用してください')
+      end
+      it 'first_nameは漢字・カタカナ・ひらがなでないと登録できない' do
+        @user.first_name = '000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('名前全角文字を使用してください')
+      end
+      it 'kana_last_nameは全角カタカナでないと登録できない' do
+        @user.kana_last_name = '000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('姓（カナ）全角カタカナのみで入力して下さい')
+      end
+      it 'kana_first_nameは全角カタカナでないと登録できない' do
+        @user.kana_first_name = '000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('名前（カナ）全角カタカナのみで入力して下さい')
+      end
+    end
+  end
+end
