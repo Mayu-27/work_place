8217a262fe0ed405b97cf7f012f689c461cf4133
Mayu-27 <62289802+Mayu-27@users.noreply.github.com>
2021-04-15 13:48:14 +0900

    gimei導入

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,13 +1,17 @@
 FactoryBot.define do
   factory :user do
+    transient do
+      person { Gimei.name }
+    end  
+
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
     password              {'test000'}
     password_confirmation {password}
-    last_name             {Faker::Japanese::Name.last_name}
-    first_name            {Faker::Japanese::Name.first_name}
-    kana_last_name        {'ヤマダ'}
-    kana_first_name       {'タロウ'}
+    last_name             {person.last.kanji}
+    first_name            {person.first.kanji}
+    kana_last_name        {person.last.katakana}
+    kana_first_name       {person.first.katakana}
     birth_date            {Faker::Date.birthday}
   end
 end
