43c414e2a26e0ac70b8ca0e531dfd6b48f6e35fd
Mayu-27 <62289802+Mayu-27@users.noreply.github.com>
2021-04-26 21:29:23 +0900

    chromeのoption修正

--- app/models/accea_cafe_scraping.rb
+++ app/models/accea_cafe_scraping.rb
@@ -2,11 +2,15 @@ module AcceaCafeScraping
   require 'selenium-webdriver'
 
   options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--no-sandbox')
-  options.add_argument('--headless')
-  options.add_argument('--disable-gpu')
-  options.add_argument("--disable-dev-shm-usage"); 
-  options.add_argument("--remote-debugging-port=9222") 
+  # options.add_argument('--no-sandbox')
+  # options.add_argument('--headless')
+  # options.add_argument('--disable-gpu')
+  # options.add_argument("--disable-dev-shm-usage"); 
+  # options.add_argument("--remote-debugging-port=9222") 
+
+  options.add_argument("no-sandbox")
+  options.add_argument("--disable-extensions")
+  options.add_argument("--headless")
 
   def set_accea_cafes
     driver = Selenium::WebDriver.for :chrome, options: options
