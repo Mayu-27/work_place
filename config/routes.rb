d6721618142766dbbc39069fa5842bddc5a18717
Mayu-27 <62289802+Mayu-27@users.noreply.github.com>
2021-06-05 19:04:57 +0900

    不要なルーティング削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ Rails.application.routes.draw do
   root to: 'spots#index'
   resources :users, only: [:show] 
   resources :spots do
-    resources :reviews, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :reviews
     resource :favorites, only: [:create, :destroy]
   end
-  post '/spots/:spot_id/reviews/:id/edit', to: 'reviews#update'
-  delete '/spots/:spot_id/reviews/:id', to: 'reviews#destroy'
   get 'search', to: 'spots#search'
 end
 
